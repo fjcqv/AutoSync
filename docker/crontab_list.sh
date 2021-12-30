e0b8ac5359328442f63645e39ad1d70a3a95101b
LingFeng <lingfeng_0918@163.com>
2021-12-30 11:53:46 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -88,6 +88,8 @@
 0 0,18 * * * node /scripts/jd_mall_active.js >> /scripts/logs/jd_mall_active.log 2>&1
 #超级无线店铺签到
 31 1 * * * node /scripts/jd_sevenDay.js >> /scripts/logs/jd_sevenDay.log 2>&1
+#京东通天塔--签到
+31 8 * * * node /scripts/jd_m_sign.js >> /scripts/logs/jd_m_sign.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
